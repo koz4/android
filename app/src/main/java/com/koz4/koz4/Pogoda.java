c75ac7aaac546c34443852450f9c511ec08f829c
koz4 <koza_to_nie@o2.pl>
2016-09-18 22:31:22 +0200

    trzeci comit

--- app/src/main/java/com/koz4/koz4/Pogoda.java
+++ app/src/main/java/com/koz4/koz4/Pogoda.java
@@ -3,7 +3,7 @@ package com.koz4.koz4;
 import com.firebase.client.Firebase;
 
 /**
- * Created by Marek on 9/18/2016.
+ * Created by Marek Kuzioła on 9/18/2016.
  */
 public class Pogoda extends android.app.Application {
     @Override
